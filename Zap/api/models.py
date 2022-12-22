d0c3b80bfa5c67800571f410a65e2e45dd8233f0
Anirban Sikdar <anirban9293@gmail.com>
2022-12-22 20:35:55 +0530

    Updating models.py

---
+++ Zap/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+import string
+import random
+
+# Create your models here.
+
+def generate_unique_code():
+    length=6
+    while True:
+        code = ''.join(random.choices(string.ascii_lowercase, k=length))
+        if User.objects.filter(code=code).count() == 0 and Stat.objects.filter(code=code).count() == 0:
+            break
+        
+    return code
+
+class User(models.Model):
+    code=models.CharField(max_length=8,default=generate_unique_code ,unique=True)
+    stat=models.CharField(max_length=8 ,unique=True)#Status of the id of the charging station if engaged None if not engaged
+    phone = models.IntegerField(max_length=10, unique=True)
+    charge_stat = models.IntegerField(max_length=3, default=0,null=False)
+
+class Stat(models.Model):
+    stat = models.CharField(max_length=8,default=generate_unique_code ,unique=True)
+    user = models.CharField(max_length=8, unique=True)
+    long = models.DecimalField(max_digits=30, decimal_places=15)
+    lat = models.DecimalField(max_digits=30, decimal_places=15)
+    lock_stat = models.BooleanField(null=False, default=False)#status if lock is engaged or disengaged
