9830aa139eade5c8a3b2398719f2097eb411b0c5
Anirban Sikdar <anirban9293@gmail.com>
2023-01-01 16:06:16 +0530

    Adding traditional login and signup

--- Zap/Zap/settings.py
+++ Zap/Zap/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'api.apps.ApiConfig',
     'rest_framework_simplejwt',
-    'wallet.apps.WalletConfig'
+    'wallet.apps.WalletConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
--- Zap/api/urls.py
+++ Zap/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import RegisterView, MyObtainTokenPairView
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.contrib.auth import views as auth_views
+from api import views as user_views
 
 urlpatterns = [
-    path('', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view()),
+    
+    path('login/', auth_views.LoginView.as_view(template_name='api/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='api/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
 ]
